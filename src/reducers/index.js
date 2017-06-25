4fb410e86706a205f367370f8dbf48762ef88f2e
DESKTOP-V1FC54559215 <592151648@qq.com>
2017-06-25 16:38:24 +0800

    add：修改图片

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import counter from './counter'
-// import login from './login'
-//使用redux的combineReducers方法将所有reducer打包起来
-const rootReducer = combineReducers({
-  counter
-  // ,login
-})
-export default rootReducer
