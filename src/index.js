e3cf3cdc5165ccf2d2415dbf4cb592aa485c13ec
DESKTOP-V1FC545\59215 <592151648@qq.com>
2017-06-25 18:23:38 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -15,21 +15,29 @@ ReactDOM.render((
     </Router>
 ),document.getElementById('app'));*/
 
-import React from 'react'
-import { render } from 'react-dom'
-// import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route,browserHistory } from 'react-router-dom'
-import Login from './components/Login'
-import APP from './components/Main'
-// import configureStore from './stores/configureStore'
+// import React from 'react'
+// import { render } from 'react-dom'
+// // import { Provider } from 'react-redux'
+// import { BrowserRouter as Router, Route,browserHistory,IndexRoute } from 'react-router-dom'
+// import Login from './components/Login'
+// import APP from './components/Main'
+// // import configureStore from './stores/configureStore'
 
-render(
-    <Router history={browserHistory}>
-        <div>
-            <Route path="/Login" component={Login}/>
-            <Route path="/APP" component={APP}/>
-        </div>
-    </Router>,
-  document.getElementById('root')
-)
+// render(
+//     <Router history={browserHistory}>
+//         <div>
+//             <Route path="/" component={Login}/>
+//             <IndexRoute component={Login}/>
+//             <Route path="/APP" component={APP}/>
+//         </div>
+//     </Router>,
+//   document.getElementById('root')
+// )
 
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main';
+
+// Render the main component into the dom
+ReactDOM.render(<App />, document.getElementById('root'));
