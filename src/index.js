dfbe4f284502e312d339a7dcb22e11dbcfcaf704
DESKTOP-V1FC545\59215 <592151648@qq.com>
2017-06-25 17:54:39 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -17,22 +17,19 @@ ReactDOM.render((
 
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route,browserHistory } from 'react-router-dom'
 import Login from './components/Login'
 import APP from './components/Main'
-import configureStore from './stores/configureStore'
-const store = configureStore()
+// import configureStore from './stores/configureStore'
 
 render(
-    <Provider store={store} >
-        <Router history={browserHistory}>
-            <div>
-                <Route path="/Login" component={Login}/>
-                <Route path="/APP" component={APP}/>
-            </div>
-        </Router>
-    </Provider>,
+    <Router history={browserHistory}>
+        <div>
+            <Route path="/Login" component={Login}/>
+            <Route path="/APP" component={APP}/>
+        </div>
+    </Router>,
   document.getElementById('root')
 )
 
